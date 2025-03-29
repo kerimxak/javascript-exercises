0c45b6945e5dda124cfbca3a8aa06fccd845335f
kerimxak <kerimxak@icloud.com>
2025-03-29 14:27:07 +0300

    Return reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let array = word.split("")
+  let reverseArray = []
+  for (let index = 0; index < array.length; index++) {
+    reverseArray[array.length - 1 - index] = array[index]
+    
+  }
+  
+  let reverseWord = reverseArray.join("")
+  return reverseWord
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
