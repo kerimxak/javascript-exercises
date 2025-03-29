875af3b759e2013ace0009e245c269f70b39bb45
kerimxak <kerimxak@icloud.com>
2025-03-29 12:48:29 +0300

    Make a function to return input looped

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToBeRepeated, howManyTimes) {
+  let result = ''
+  if (howManyTimes < 0){
+    return "ERROR"
+  }
+  for (let i = 0; i < howManyTimes; i++) {
+    result += stringToBeRepeated
+    
+  }
+  return result
 };
 
 // Do not edit below this line
