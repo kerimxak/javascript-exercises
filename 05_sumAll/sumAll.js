c6f12348825c230d8f15efd47eecd6341c07627f
kerimxak <kerimxak@icloud.com>
2025-03-30 15:04:55 +0300

    Make a function to add all numbers inbetween

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let answer = 0
+  let from = 0
+  let to = 0
+  if (num1 < num2) {
+    from = num1
+    to = num2
+  } else {
+    from = num2
+    to = num1
+  }
 
+  if (Number.isInteger(from) && Number.isInteger(to) && from >= 0 && to >= 0){
+    for (let i = from; i <= to; i++) {
+      answer += i
+      
+    }
+  } else {
+    return "ERROR"
+  }
+  return answer
 };
 
 // Do not edit below this line
