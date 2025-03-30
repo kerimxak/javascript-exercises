99a75166af12ab7c5547faa5e6145b5366001611
kerimxak <kerimxak@icloud.com>
2025-03-30 14:06:23 +0300

    Make a function to remove specific elements in arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
 const removeFromArray = function() {
+  let number = 0
+
+  for (let i = 1; i < arguments.length; i++) {
+    for (let j = 0; j < arguments[0].length; j++) {
+      if (arguments[0][j] === arguments[i]) {
+        arguments[0].splice(j,1)
+        j--
+      }
+        
+      
+    }
+    
+  }
+
+  return arguments[0]
 };
 
 // Do not edit below this line
